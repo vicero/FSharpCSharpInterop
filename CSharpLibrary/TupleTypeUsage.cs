4c67b457a258b3700fb71435eac85bdcf5738d5d
James Kessler <james.kessler@gmail.com>
2018-04-27 11:30:24 -0600

    Add Tuple usage examples and reference to System.ValueTuple

---
+++ CSharpLibrary/TupleTypeUsage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using FSharpLibrary;
+
+using Xunit;
+
+namespace CSharpLibrary
+{
+    public static class TupleTypeUsage
+    {
+        [Fact]
+        public static void TupleTypeClass_IsSystemTuple()
+        {
+            var tuple = TupleType.CreateNameAndEmail("John Smith", "a@b.com");
+            Assert.IsType<Tuple<string, string>>(tuple);
+        }
+
+        [Fact]
+        public static void TupleTypeClass_IsValueTuple()
+        {
+            var tuple = TupleType.CreateStructNameAndEmail("John Smith", "a@b.com");
+            Assert.IsType<ValueTuple<string, string>>(tuple);
+        }
+    }
+}
---
+++ FSharpLibrary/TupleType.fs
@@ -0,0 +1,5 @@
+module FSharpLibrary.TupleType
+
+let CreateNameAndEmail name email = (name, email)
+
+let CreateStructNameAndEmail name email = struct (name, email)
