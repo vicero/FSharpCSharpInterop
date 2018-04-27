32aafd79176a4f2c6d7b58cb77a4c652513cd193
James Kessler <james.kessler@gmail.com>
2018-04-27 09:32:28 -0600

    Initial solution and project setup

---
+++ FSharpLibrary/Library.fs
@@ -0,0 +1,9 @@
+﻿namespace FSharp
+
+
+// Regular Single Case Union
+type EmailAddress = EmailAddress of string
+
+// Struct version of the above
+[<Struct>]
+type StructEmailAddress = EmailAddress of string
